931a5edd0aeac5694cabe761d5f665e5b8ee3eef
thiagohwangg <cmnqpart2@gmail.com>
2024-05-26 23:38:17 +0700

    lesson7.1 the rest with ORM

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import mysql from "mysql2/promise";
 import bluebird from "bluebird";
-import db from "../models/"
+import db from "../models/";
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -13,85 +13,101 @@ const hashUserPassword = (userPassword) => {
 const createNewUser = async (email, password, username) => {
   let hashPass = hashUserPassword(password);
   try {
-   await db.User.create({
+    await db.User.create({
       username: username,
       email: email,
-      password: hashPass
-    })
+      password: hashPass,
+    });
   } catch (error) {
     console.log("error: ", error);
   }
 };
 
 const getUserList = async () => {
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "jwt",
-    Promise: bluebird,
-  });
+  let users = [];
 
-  try {
-    const [rows, fields] = await connection.execute("Select * from user");
-    return rows;
-  } catch (error) {
-    console.log("error: ", error);
-  }
+  users = await db.User.findAll();
+
+  return users;
+
+  // try {
+
+  // } catch (error) {
+  //   console.log("error: ", error);
+  // }
 };
 
-const deleteUser = async(id) => {
-  
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "jwt",
-    Promise: bluebird,
+const deleteUser = async (userId) => {
+  await db.User.destroy({
+    where: { id: userId },
   });
-  
-  try {
-    const [rows, fields] = await connection.execute('DELETE FROM user WHERE id=?', [id]);
-    return rows;
-  } catch (error) {
-    console.log("error: ", error);
-  }
-}
+  // const connection = await mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   database: "jwt",
+  //   Promise: bluebird,
+  // });
 
-const getUserById = async(id) => {
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "jwt",
-    Promise: bluebird,
-  });
-  
-  try {
-    const [rows, fields] = await connection.execute('Select * FROM user WHERE id=?', [id]);
-    return rows;
-  } catch (error) {
-    console.log("error: ", error);
-  }
-}
+  // try {
+  //   const [rows, fields] = await connection.execute('DELETE FROM user WHERE id=?', [id]);
+  //   return rows;
+  // } catch (error) {
+  //   console.log("error: ", error);
+  // }
+};
 
-const updateUserInfo = async(email, username, id) => {
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "jwt",
-    Promise: bluebird,
+const getUserById = async (id) => {
+  let user = {};
+
+  user = await db.User.findOne({
+    where: { id: id },
   });
-  
-  try {
-    const [rows, fields] = await connection.execute('UPDATE user SET email = ?, username = ? WHERE id = ?', [email, username, id]);
-    return rows;
-  } catch (error) {
-    console.log("error: ", error);
-  }
-}
+
+  return user.get({ plain: true });
+  // const connection = await mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   database: "jwt",
+  //   Promise: bluebird,
+  // });
+
+  // try {
+  //   const [rows, fields] = await connection.execute('Select * FROM user WHERE id=?', [id]);
+  //   return rows;
+  // } catch (error) {
+  //   console.log("error: ", error);
+  // }
+};
+
+const updateUserInfo = async (email, username, id) => {
+  await db.User.update(
+    {
+      email: email,
+      username: username,
+    },
+    {
+      where: { id: id },
+    }
+  );
+  // const connection = await mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   database: "jwt",
+  //   Promise: bluebird,
+  // });
+
+  // try {
+  //   const [rows, fields] = await connection.execute('UPDATE user SET email = ?, username = ? WHERE id = ?', [email, username, id]);
+  //   return rows;
+  // } catch (error) {
+  //   console.log("error: ", error);
+  // }
+};
 
 module.exports = {
   createNewUser,
   getUserList,
   deleteUser,
   getUserById,
-  updateUserInfo
+  updateUserInfo,
 };
