93008d2edd68645493cfbba9b9a6c56cd8a565b7
thiagohwangg <cmnqpart2@gmail.com>
2024-06-11 23:52:03 +0700

    lesson 10.6.22 Pagination React & Node JS

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,62 +1,74 @@
-import userApiService from '../services/userApiService'
+import userApiService from "../services/userApiService";
 
-const readFunc = async(req, res) => {
-    try {
-        let data = await userApiService.getAllUser()
-        return res.status(200).json({
-            EM: data.EM, //err message
-            EC: data.EC, //err code
-            DT: data.DT, //data
-        })
-    } catch (error) {
-        console.log("error: ", error);
-        return res.status(500).json({
-            EM: 'error from server', //err message
-            EC: '-1', //err code
-            DT: '', //data
-        })
+const readFunc = async (req, res) => {
+  try {
+    if (req.query.page && req.query.limit) {
+      let page = req.query.page;
+      let limit = req.query.limit;
+
+      let data = await userApiService.getUserWithPagination(+page, +limit);
+      return res.status(200).json({
+        EM: data.EM, //err message
+        EC: data.EC, //err code
+        DT: data.DT, //data
+      });
+    } else {
+      let data = await userApiService.getAllUser();
+      return res.status(200).json({
+        EM: data.EM, //err message
+        EC: data.EC, //err code
+        DT: data.DT, //data
+      });
     }
-}
+  } catch (error) {
+    console.log("error: ", error);
+    return res.status(500).json({
+      EM: "error from server", //err message
+      EC: "-1", //err code
+      DT: "", //data
+    });
+  }
+};
 
 const createFunc = (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log("error: ", error);
-        return res.status(500).json({
-            EM: 'error from server', //err message
-            EC: '-1', //err code
-            DT: '', //data
-        })
-    }
-}
+  try {
+  } catch (error) {
+    console.log("error: ", error);
+    return res.status(500).json({
+      EM: "error from server", //err message
+      EC: "-1", //err code
+      DT: "", //data
+    });
+  }
+};
 
 const updateFunc = (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log("error: ", error);
-        return res.status(500).json({
-            EM: 'error from server', //err message
-            EC: '-1', //err code
-            DT: '', //data
-        })
-    }
-}
+  try {
+  } catch (error) {
+    console.log("error: ", error);
+    return res.status(500).json({
+      EM: "error from server", //err message
+      EC: "-1", //err code
+      DT: "", //data
+    });
+  }
+};
 
 const deleteFunc = (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log("error: ", error);
-        return res.status(500).json({
-            EM: 'error from server', //err message
-            EC: '-1', //err code
-            DT: '', //data
-        })
-    }
-}
+  try {
+  } catch (error) {
+    console.log("error: ", error);
+    return res.status(500).json({
+      EM: "error from server", //err message
+      EC: "-1", //err code
+      DT: "", //data
+    });
+  }
+};
 
 module.exports = {
-    readFunc, createFunc, updateFunc, deleteFunc
-}
+  readFunc,
+  createFunc,
+  updateFunc,
+  deleteFunc,
+};
--- src/services/userApiService.js
+++ src/services/userApiService.js
@@ -29,6 +29,39 @@ const getAllUser = async () => {
   }
 };
 
+const getUserWithPagination = async(page, limit) => {
+  try {
+    let offset= (page - 1) *limit;
+
+    const {count, rows} = await db.User.findAndCountAll({
+      offset: offset,
+      limit: limit
+    })
+
+    let totalPages = Math.ceil(count / limit)
+
+    let data = {
+      totalRows: count,
+      totalPages: totalPages,
+      users: rows
+      }
+
+    return {
+      EM: "fetch OK",
+      EC:0,
+      DT: data,
+    };
+
+} catch (error) {
+    console.log("error: ", error);
+    return {
+      EM: "something wrongs with services",
+      EC: 1,
+      DT: [],
+    };
+}
+}
+
 const createNewUser = async(data) => {
     try {
         await db.User.create({
@@ -76,4 +109,5 @@ module.exports = {
   createNewUser,
   updateUser,
   deleteUser,
+  getUserWithPagination
 };
