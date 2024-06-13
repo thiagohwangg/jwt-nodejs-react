3790a9c431212da3d689f7659fae19ef7ac7aedd
thiagohwangg <cmnqpart2@gmail.com>
2024-06-13 22:44:35 +0700

    lesson 10.6.5 Create New User

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -30,8 +30,14 @@ const readFunc = async (req, res) => {
   }
 };
 
-const createFunc = (req, res) => {
+const createFunc = async(req, res) => {
   try {
+    let data = await userApiService.createNewUser(req.body);
+      return res.status(200).json({
+        EM: data.EM, //err message
+        EC: data.EC, //err code
+        DT: data.DT, //data
+      });
   } catch (error) {
     console.log("error: ", error);
     return res.status(500).json({
